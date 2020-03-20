8d90c916f7b5f694765259b258b6554a9535d8ce
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-20 09:14:45 -0500

    added consolelolec.java file

---
+++ src/HelloWorld.java
@@ -0,0 +1,59 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int myFavoriteNumber = 5;
+        String myString = "wazzup fam";
+        long myNumber = 123;
+        double decimal = 3.14;
+
+        System.out.println("Hello World!");
+        System.out.print("Hi ");
+        System.out.print("world\n");
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+        System.out.println(decimal);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+
+//        int x = 4;
+//        x = x + 5;
+
+        //int x = 4;
+//          x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//          y *= 4;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//          y /= x;
+//          y -= x;
+//        System.out.println(y);
+
+        System.out.println(Integer.MAX_VALUE);
+
+
+
+    }
+}
