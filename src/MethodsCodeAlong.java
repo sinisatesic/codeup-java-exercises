0ff91d5f858527837c3a5e0fff609069c58b25f2
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-23 13:44:14 -0500

    completed coding along on methodscodealong java file

--- src/MethodsCodeAlong.java
+++ src/MethodsCodeAlong.java
@@ -21,10 +21,24 @@ public class MethodsCodeAlong {
     // TODO: create a main function
 
     public static void main(String[] args) {
+        System.out.println("\nV1");
         sayHello(3);
+
+
+        System.out.println("\nV2");
+        sayHello();
+
+        System.out.println("\nV3");
+        sayHello("\nGoodbye!");
+
+        System.out.println("\nV4");
+        sayHello("Salutations my","friend!");
+
     }
     // TODO: Call version 1 so that it prints "Hello World!" three times.
+    //in main method
     // TODO: Call version 2.
+    //in main method
     // TODO: Call version 3 so that is says "Hello" on one line and "Goodbye!" on the second line.
     // TODO: Call version 4 so that is says, "Salutations my friend!"
 
