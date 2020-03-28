285c906fb97c5fb81c427e382c955e4008bf4d94
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-27 22:32:45 -0500

    added youtube practice 1 java file

---
+++ src/YoutubePractice1.java
@@ -0,0 +1,10 @@
+public class YoutubePractice1 {
+
+    public static void main(String[] args){
+        System.out.println("Home Town Hamburger Restaurant");
+        System.out.println("Hours:\tMon - Fri, 9am - 9pm");
+        System.out.println("\nLowest Prices in Town!");
+        System.out.println("\tLunch\tDinner");
+        System.out.println("\t$5.99\t$11.95");
+    }
+}
