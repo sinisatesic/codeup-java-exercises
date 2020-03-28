f5aeb3e015c57f95d8f23aa834a1ec96698462bb
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-27 23:14:34 -0500

    added youtube practice 2 java file

---
+++ src/YoutubePractice2.java
@@ -0,0 +1,22 @@
+public class YoutubePractice2 {
+
+    public static void main(String[] args){
+        int variable1;
+        int variable2, variable3;
+
+        variable1 = 9;
+        variable2 = 24;
+        variable3 = 16;
+
+        System.out.println("Variable is : " + variable1);
+        System.out.println("The second variable is :" + variable2 + " and the third variable is: " + variable3);
+
+        System.out.println(variable1 + variable2);
+
+        System.out.println(variable1 * variable2);
+
+        System.out.println(variable2 / variable1); // will print two because int has no decimals
+
+        System.out.println(variable2 % variable1);
+    }
+}
