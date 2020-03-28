21bf41d03b24087af7a5565d57eeccbebe7eaa17
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-27 23:50:28 -0500

    added youtube practice 4

---
+++ src/YoutubePractice3.java
@@ -0,0 +1,15 @@
+public class YoutubePractice3 {
+
+    public static void main(String[] args){
+
+        //write a program that declares three variables name ag1, age2, and age3.
+        //assign any age you wish to each of the three variables, then
+        //use the println() method to output the ages to the screen separated by commas.
+
+        int age1 = 3;
+        int age2 = 6;
+        int age3 = 9;
+
+        System.out.println("First age is " + age1 + ", the second age is " + age2 + ", and third age is " + age3);
+    }
+}
---
+++ src/YoutubePractice4.java
@@ -0,0 +1,20 @@
+public class YoutubePractice4 {
+
+    public static void main(String[] args){
+        //Lisa has 34 dollars, John has 205 dollars, and Jenny has negative 20 dollars
+        //Write a program that assigns these dollar amounts to variables, then
+        //output the following to the screen, exactly as written.
+
+        //Lisa has 34 dollars, and John has 205 dollars.
+        //Jenny has negative money, and she has -20 dollars.
+
+        int lisa, john, jenny;
+
+        lisa = 34;
+        john = 205;
+        jenny = -20;
+
+        System.out.println("Lisa has " + lisa + " dollars, and John has " + john + " dollars. Jenny has negative money, and she has " + jenny + " dollars.");
+    }
+
+}
